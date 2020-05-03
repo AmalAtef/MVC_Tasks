1a5cdbaf550ae0b794fc88af7da0cbb250e9b41c
AmalAtef <AmalAtef@users.noreply.github.com>
2020-05-03 15:55:26 +0200

    Task Day1

---
+++ MVCDay1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDay1.Controllers
+{
+    public class User{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Choice { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Form()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Form(User user)
+        {
+            if (user.Name!= null && user.Email != null)
+            {
+                ViewBag.Name = user.Name;
+                return View("Welcome");
+            }
+            return View();
+        }
+
+    }
+}
